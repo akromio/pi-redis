af4a6e53ba901d94ae1a6e534d72f4da49436ef1
siakro <siakro@users.noreply.github.com>
2022-11-22 12:08:24 +0100

    tests: added unit and itg tests

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,7 +3,7 @@
 var _core = require("@dogmalang/core");
 const {
   createClient
-} = _core.dogma.use(require("redis"));
+} = _core.dogma.use(require("@redis/client"));
 module.exports = exports = {
   ["plugin"]: "redis",
   ["desc"]: "Akromio plugin for working with Redis.",
@@ -18,17 +18,17 @@ async function initialize(args) {
   let state = {}; /* c8 ignore next */
   _core.dogma.expect("args", args);
   {
-    var _args$host, _args$port, _opts$readonly;
+    var _args$host, _args$port, _args$readonly;
     const client = createClient(Object.assign({}, {
       ["socket"]: {
         ["host"]: (_args$host = args.host) !== null && _args$host !== void 0 ? _args$host : "localhost",
         ["port"]: (_args$port = args.port) !== null && _args$port !== void 0 ? _args$port : 6379
       },
-      ["readonly"]: (_opts$readonly = opts.readonly) !== null && _opts$readonly !== void 0 ? _opts$readonly : false
-    }, opts.username ? {
-      ["username"]: opts.username
-    } : {}, opts.password ? {
-      ["password"]: opts.password
+      ["readonly"]: (_args$readonly = args.readonly) !== null && _args$readonly !== void 0 ? _args$readonly : false
+    }, args.username ? {
+      ["username"]: args.username
+    } : {}, args.password ? {
+      ["password"]: args.password
     } : {}));
     0, await client.connect();
     _core.dogma.update(state, {
